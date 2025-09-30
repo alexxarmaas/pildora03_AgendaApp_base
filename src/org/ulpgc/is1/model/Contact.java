0592cfcd70e8dd7803b85241864608cfad3ae918
Alejandro <alejandro.armas103@alu.ulpgc.es>
2025-09-30 19:20:19 +0100

    feat: Added contact

--- src/org/ulpgc/is1/model/Contact.java
+++ src/org/ulpgc/is1/model/Contact.java
@@ -2,4 +2,31 @@ package org.ulpgc.is1.model;
 
 public abstract class Contact {
 
+    public String telephone;
+    public String email;
+
+    public Contact(String telephone, String email) {
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
